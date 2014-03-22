98d33400977112d7bb43eb8e49e8d1ebb2dc3c9d
josejamilena <josejamilena@gmail.com>
2014-03-22 17:50:45 +0100

    ejemplo en ruby

---
+++ ejemplo1.rb
@@ -0,0 +1,68 @@
+require 'net/http'
+
+module Couch
+
+  class Server
+    def initialize(host, port, options = nil)
+      @host = host
+      @port = port
+      @options = options
+    end
+
+    def delete(uri)
+      request(Net::HTTP::Delete.new(uri))
+    end
+
+    def get(uri)
+      request(Net::HTTP::Get.new(uri))
+    end
+
+    def put(uri, json)
+      req = Net::HTTP::Put.new(uri)
+      req["content-type"] = "application/json"
+      req.body = json
+      request(req)
+    end
+
+    def post(uri, json)
+      req = Net::HTTP::Post.new(uri)
+      req["content-type"] = "application/json"
+      req.body = json
+      request(req)
+    end
+
+    def request(req)
+      res = Net::HTTP.start(@host, @port) { |http|http.request(req) }
+      unless res.kind_of?(Net::HTTPSuccess)
+        handle_error(req, res)
+      end
+      res
+    end
+
+    private
+
+    def handle_error(req, res)
+      e = RuntimeError.new("#{res.code}:#{res.message}\nMETHOD:#{req.method}\nURI:#{req.path}\n#{res.body}")
+      raise e
+    end
+  end
+end
+
+
+#Creating a Database
+server = Couch::Server.new("localhost", "5984")
+server.put("/foo/", "")
+#Creating a Document
+server = Couch::Server.new("localhost", "5984")
+doc = <<-JSON
+{"type":"comment","body":"First Post!"}
+JSON
+server.put("/foo/document_id", doc)
+#Reading a Document
+server = Couch::Server.new("localhost", "5984")
+res = server.get("/foo/document_id")
+json = res.body
+puts json
+#Deleting a Database
+#server = Couch::Server.new("localhost", "5984")
+#server.delete("/foo")
